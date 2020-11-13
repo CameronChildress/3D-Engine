ff0aa45f5bc2ea9e142497b2c6b9a369cef29cf8
CameronChildress <cameronchildress4@gmail.com>
2020-11-13 09:10:11 -0700

    Added Engine Files

---
+++ Application/Engine/Graphics/Texture.cpp
@@ -0,0 +1,32 @@
+#include "pch.h"
+#include "Texture.h"
+#include "Renderer.h"
+
+namespace nc
+{
+	bool nc::Texture::Create(const std::string& name, void* renderer)
+	{
+		m_renderer = static_cast<Renderer*>(renderer)->m_renderer;
+
+		SDL_Surface* surface = nullptr; // IMG_Load(name.c_str());
+		if (surface == nullptr)
+		{
+			return false;
+		}
+
+		m_texture = SDL_CreateTextureFromSurface(m_renderer, surface);
+		SDL_FreeSurface(surface);
+
+		if (m_texture == nullptr)
+		{
+			return false;
+		}
+
+		return true;
+	}
+
+	void nc::Texture::Destroy()
+	{
+		SDL_DestroyTexture(m_texture);
+	}
+}
